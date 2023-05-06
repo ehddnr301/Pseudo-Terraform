b44d68767b5f4cb5bb6e712782d972254f386f12
ehddnr301 <dy950328@gmail.com>
2023-05-06 12:41:15 +0900

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,40 @@
+data "template_file" "ssh_key" {
+  template = file("${path.module}/assets/my_key.pub")
+}
+
+
+provider "google" {
+  project     = var.my_project_id
+  region      = "us-west4-b"
+  zone        = "us-west4-b"
+  credentials = file("${path.module}/assets/${var.my_project_id}.json")
+}
+
+resource "google_compute_instance" "vm_instance" {
+  count        = 1
+  name         = "terraform-instance-${count.index}"
+  machine_type = "e2-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+      size  = 50
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      // add a public IP address for the instance
+      // or use nat_ip parameter to specify a static external IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.iam_user_name}:${data.template_file.ssh_key.rendered}"
+  }
+}
+
+output "MYOUTPUT" {
+  value = data.template_file.ssh_key.rendered
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+iam_user_name = "dongwook.test9"
+my_project_id = "pseudo-de4e-123456"
